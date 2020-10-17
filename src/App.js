2908caf92f993767fa662a65f2abe24acde72106
fouad <Chamma.fouad@gmail.com>
2020-10-17 15:15:58 +1100

    inc bootstrap pckg, react-router, nav structure

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
 import React from "react";
-// import Nav from "./Nav";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Nav from "./components/Nav";
+import "bootstrap/dist/css/bootstrap.css";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>Broccoli & Co.</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Nav />
+        <header className="App-header">
+          <p>Broccoli & Co.</p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    </Router>
   );
 }
 
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,1 +1,29 @@
-<nav></nav>;
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import * as ReactBootstrap from "react-bootstrap";
+
+function Nav() {
+  return (
+    <ReactBootstrap.Navbar className="nav mt-5 sticky-top" expand="lg">
+      <ReactBootstrap.Navbar.Brand href="/">
+        <div className="nav__logo"></div>
+      </ReactBootstrap.Navbar.Brand>
+      <ReactBootstrap.Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <ReactBootstrap.Navbar.Collapse id="basic-navbar-nav">
+        <ReactBootstrap.Nav>
+          <Link to="/" className="nav__nav-link">
+            Work
+          </Link>
+          <Link to="/about" className="nav__nav-link">
+            About
+          </Link>
+          <Link to="/contact" className="nav__nav-link">
+            Contacts
+          </Link>
+        </ReactBootstrap.Nav>
+      </ReactBootstrap.Navbar.Collapse>
+    </ReactBootstrap.Navbar>
+  );
+}
+
+export default Nav;
